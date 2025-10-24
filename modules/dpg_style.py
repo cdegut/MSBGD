0e8a2b9ce4a59227299975dea6cd30b2bb557480
Clément Dégut <c.degut@gmail.com>
2025-10-24 19:52:00 +0100

    style for derivative

--- modules/dpg_style.py
+++ modules/dpg_style.py
@@ -8,6 +8,11 @@ def create_styles():
             dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
             dpg.add_theme_style(dpg.mvPlotStyleVar_LineWeight, 2, category=dpg.mvThemeCat_Plots)
 
+    with dpg.theme(tag="derivative2nd_theme"):
+        with dpg.theme_component(dpg.mvAll):
+            dpg.add_theme_style(dpg.mvPlotStyleVar_LineWeight, 1, category=dpg.mvThemeCat_Plots)
+            dpg.add_theme_color(dpg.mvPlotCol_Line, (48,70,116), category=dpg.mvThemeCat_Plots)
+    
     with dpg.theme(tag="data_theme"):
         with dpg.theme_component(dpg.mvAll):
             dpg.add_theme_style(dpg.mvPlotStyleVar_LineWeight, 1, category=dpg.mvThemeCat_Plots)
@@ -15,12 +20,12 @@ def create_styles():
 
     with dpg.theme(tag = "filtered_data_theme"):
         with dpg.theme_component(dpg.mvAll):
-            dpg.add_theme_style(dpg.mvPlotStyleVar_LineWeight, 1, category=dpg.mvThemeCat_Plots)
+            dpg.add_theme_style(dpg.mvPlotStyleVar_LineWeight, 2, category=dpg.mvThemeCat_Plots)
             dpg.add_theme_color(dpg.mvPlotCol_Line, (250,120,14), category=dpg.mvThemeCat_Plots)
 
     with dpg.theme(tag = "baseline_theme"):
         with dpg.theme_component(dpg.mvAll):
-            dpg.add_theme_style(dpg.mvPlotStyleVar_LineWeight, 1, category=dpg.mvThemeCat_Plots)
+            dpg.add_theme_style(dpg.mvPlotStyleVar_LineWeight, 2, category=dpg.mvThemeCat_Plots)
             dpg.add_theme_color(dpg.mvPlotCol_Line, (195, 242, 197), category=dpg.mvThemeCat_Plots)
     
     with dpg.theme(tag = "matching_MBG_theme"):
